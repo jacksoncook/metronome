7c016d4008fe9ad4dfbdee1deadf589a02f3cd65
Altan <astalker3@gatech.edu>
2019-06-09 18:27:21 -0400

    Changed main class name to Metronome

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(Metronome());
 
-class MyApp extends StatelessWidget {
+class Metronome extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'metri:beat',
       theme: ThemeData(
         // This is the theme of your application.
         //
