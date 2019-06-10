9ca56b6021a19618daef511af80fbe0aa5dfd639
Altan <astalker3@gatech.edu>
2019-06-09 20:33:15 -0400

    Converted project structure to blocs? maybe

---
+++ lib/blocs/counter/counter_bloc.dart
@@ -0,0 +1,27 @@
+import 'package:bloc/bloc.dart';
+import 'package:metronome/blocs/counter/counter_event.dart';
+import 'package:metronome/blocs/counter/counter_state.dart';
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+
+  void onIncrement() {
+    this.dispatch(new IncrementEvent());
+  }
+
+  void onDecrement() {
+    this.dispatch(new DecrementEvent());
+  }
+
+  @override
+  CounterState get initialState => CounterState.initial();
+
+  @override
+  Stream<CounterState> mapEventToState(CounterEvent event,) async* {
+    final _currentState = currentState;
+    if (event is IncrementEvent) {
+      yield CounterState(counter: _currentState.counter + 1);
+    } else if (event is DecrementEvent) {
+      yield CounterState(counter: _currentState.counter - 1);
+    }
+  }
+}
